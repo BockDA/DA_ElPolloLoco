2996caf050f7e98c0f953c167b6db3e36f1ff267
Bockinger <cbock2011@gmail.com>
2025-05-03 18:22:47 +0200

    create keyboard class

--- js/game.js
+++ js/game.js
@@ -1,17 +1,59 @@
 let canvas;
-
 let world;
-
+let keyboard = new Keyboard();
 
 function init() {
     console.log("Starte");
 
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 
 
     console.log("My charater ist ", world.character);
 
-
 }
+
+
+window.addEventListener('keydown', (event) => {
+    console.log(event);
+
+    if (event.key == 39) {
+        keyboard.RIGHT = true;
+    }
+    if (event.key == 37) {
+        keyboard.LEFT = true;
+    }
+    if (event.key == 38) {
+        keyboard.UP = true;
+    }
+    if (event.key == 40) {
+        keyboard.DOWN = true;
+    }
+    if (event.key == 32) {
+        keyboard.SPACE = true;
+    }
+
+});
+
+
+window.addEventListener('keyup', (event) => {
+    console.log(event);
+
+    if (event.key == 39) {
+        keyboard.RIGHT = false;
+    }
+    if (event.key == 37) {
+        keyboard.LEFT = false;
+    }
+    if (event.key == 38) {
+        keyboard.UP = false;
+    }
+    if (event.key == 40) {
+        keyboard.DOWN = falseue;
+    }
+    if (event.key == 32) {
+        keyboard.SPACE = false;
+    }
+
+});
 
--- models/character.class.js
+++ models/character.class.js
@@ -11,7 +11,7 @@ class Character extends MovableObject {
 
     ];
 
-
+    world;
 
 
     constructor() {
