52e4fcfbc0601ee54938c951f5aa2fb6225f1f5d
Bockinger <cbock2011@gmail.com>
2025-06-10 21:36:51 +0200

    endboss attack V2

--- models/level/level.js
+++ models/level/level.js
@@ -45,7 +45,12 @@ const level1 = new Level(
         new BackgroundObjekt('img/5_background/layers/air.png', 719 * 3),
         new BackgroundObjekt('img/5_background/layers/3_third_layer/2.png', 719 * 3),
         new BackgroundObjekt('img/5_background/layers/2_second_layer/2.png', 719 * 3),
-        new BackgroundObjekt('img/5_background/layers/1_first_layer/2.png', 719 * 3)
+        new BackgroundObjekt('img/5_background/layers/1_first_layer/2.png', 719 * 3),
+
+        new BackgroundObjekt('img/5_background/layers/air.png', 719 * 4),
+        new BackgroundObjekt('img/5_background/layers/3_third_layer/2.png', 719 * 4),
+        new BackgroundObjekt('img/5_background/layers/2_second_layer/2.png', 719 * 4),
+        new BackgroundObjekt('img/5_background/layers/1_first_layer/2.png', 719 * 4)
     ],
 
     [
